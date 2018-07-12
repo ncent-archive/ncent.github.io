1db8da3f93daf18949229f47454b6938dd75295c
Joel Dominic <joeldominic@ncntadmins-MacBook-Pro.local>
2018-07-12 13:09:31 -0700

    API

--- SDK/source/ncentSDK.js
+++ SDK/source/ncentSDK.js
@@ -10,13 +10,13 @@ class ncentSDK {
     }
     // var emailAddress;
     // var emailAddress = ["", "", ""];
-
+    
     /*
-    * createWallet creates a wallet by hashing the timestamp.
+    * createWallet creates a wallet by hashing the timestamp. 
     * string walletAddress - a new walletAddress for the user.
     * string emailAddress - email.
     * string privateKey - a new privateKey for the user.
-    */
+    */   
     createWallet(emailAddress) {
        //let privateKey = ((+new Date) + Math.random()* 2).toString(32);
        //console.log(privateKey);
@@ -30,24 +30,27 @@ class ncentSDK {
             console.log(error.response.data);
         });
     };
-
+    
     /*
-        inIt creates the ncnt tokenType and the walletAccounts for
-        all NCNT employees and initializes all balances.
+        inIt creates the ncnt tokenType and the walletAccounts for 
+        all NCNT employees and initializes all balances. 
         int tokenType: UUID associated with NCNT.
-        Array<string> emailAddress: array of email addresses
+        Array<string> emailAddress: array of email addresses 
     */
     init() {
     }
-
+    
     /*
         destroyTokens destroys all tokens of a certain tokenType.
         string walletAddress: The user's wallet address.
         string privateKey: The user's private key.
-        int tokenType: The uuid associated the the token you want to destroy.
+        int tokenType: The uuid associated the the token you want to destroy. 
         success: callback;
         error: callback;
     */
+
+    //change time to server side
+
     destroyTokens(tokentype_id) {
     // checker here.
         //if (typeof walletAddress == 'object' && typeof privateKey == 'object')
@@ -66,8 +69,8 @@ class ncentSDK {
         stampTokens initiates a new token type and creates n of these tokens.
         string walletAddress: The user's wallet address.
         string signature: The user's signature.
-        string tokenName: Name of token the user wants to create.
-        date expirationDate: Date of expiration for token.
+        string tokenName: Name of token the user wants to create. 
+        date expirationDate: Date of expiration for token. 
         int numTokens: number of tokens to create.
         success: callback;
         error: callback;
@@ -78,16 +81,17 @@ class ncentSDK {
             sponsor_uuid: walletAddress,
             Name: tokenName,
             totalTokens: numTokens,
-            ExpiryDate: ExpiryDate,
+            ExpiryDate: ExpiryDate,          
         })
         .then(function(response) {
             console.log(response.data);
+            return response.data.uuid;
         })
         .catch(function(error) {
             console.log(error.response.data);
         });
     }
-
+    
     /*
         getWalletAddress gets passed in an email from the client and returns
         the wallet address associated with the email.
@@ -110,45 +114,44 @@ class ncentSDK {
     //         console.log(error);
     //     })
     //     .then(function(){
-    //     });
+    //     });     
     // }
     createBalance(wallet_id, tokentype_id) {
         axios.post(this._net + '/wallets/' + wallet_id + '/items', {
             tokentype_uuid: tokentype_id,
         })
         .then(function(response) {
-            console.log(response.data);
+            return new Promise (resolve, reject) {
+                console.log(response.data);
+                resolve(response.data.uuid);
+            };
         })
         .catch(function(error) {
             console.log(error.response.data);
-        });
-    }
-    checkIfBalanceExists(wallet_id, tokentype_id) {
-        axios.get(this._net + '/wallets/' + wallet_id + '/' + tokentype_id, {
-        })
-        .then(function(response) {
-            console.log(response.data);
-            return true;
-        })
-        .catch(function(error) {
-            console.log(error.response.data);
-            return false;
         });
     }
     /*
         transferTokens allows tokens to be transferred between two parties.
         string walletSender: The sender's wallet address.
         string signature: The sender's signature.
-        int tokenType: UUID of token the user is transferring.
+        int tokenType: UUID of token the user is transferring. 
         int tokenAmount: number of tokens to transfer.
         success: callback;
         error: callback;
     */
-    transferTokens(walletSender_id, walletReceiver_id, tokentype_id, tokenAmount) {
-        if (!this.checkIfBalanceExists(walletSender_id, tokentype_id)) this.createBalance(walletSender_id, tokentype_id);
-        if (!this.checkIfBalanceExists(walletReceiver_id, tokentype_id)) this.createBalance(walletReceiver_id, tokentype_id);
-        var sendertokenAmount = this.getTokenBalance(walletSender_id, tokentype_id);
-        var receivertokenAmount = this.getTokenBalance(walletSender_id, tokentype_id);
+    transferTokens(senderBalance_id, receiverBalance_id, walletSender_id, walletReceiver_id, tokentype_id, tokenAmount) {
+        axios.then(
+            if (!senderBalance_id) {
+                senderBalance_id = this.createBalance(walletSender_id, tokentype_id).then(function () {
+                    if (!receiverBalance_id) {
+                        receiverBalance_id = this.createBalance(walletReceiver_id, tokentype_id); 
+                    }
+                })
+            }
+        )
+
+        var sendertokenAmount = this.getTokenBalance(walletSender_id, senderBalance_id);
+        var receivertokenAmount = this.getTokenBalance(walletReceiver_id, receiverBalance_id);
         // axios.all([
         //     axios.post(this._net + '/tokentypes/' + tokentype_id + '/items', {
         //         amount: tokenAmount,
@@ -171,29 +174,31 @@ class ncentSDK {
         //     console.log(error.response.data);
         // });
     }
-
+    
     /*
-        getTokenBalance gets the balance for any user's wallet.
+        getTokenBalance gets the balance for any user's wallet. 
         string walletAddress: The user's wallet address.
-        int tokenType: The uuid associated the the token you want to destroy.
+        int tokenType: The uuid associated the the token you want to destroy. 
         success: callback;
         error: callback;
     */
-    getTokenBalance(walletAddress, tokentype_id) {
-        axios.get(this._net + '/wallets/' + walletAddress + '/' + tokentype_id, {
+    getTokenBalance(walletAddress, balance_id) {
+        axios.get(this._net + '/wallets/' + walletAddress + '/' + balance_id, {
         })
         .then(function(response) {
             console.log(response.data);
+            return response.data.amount;
         })
         .catch(function(error) {
             console.log(error.response.data);
+            return -1;
         });
     }
-
+    
     /*
-        getAllBalances returns the balance for all tokens in a user's wallet.
+        getAllBalances returns the balance for all tokens in a user's wallet. 
         string walletAddress: The user's wallet address.
-        int tokenType: The uuid associated the the token you want to destroy.
+        int tokenType: The uuid associated the the token you want to destroy. 
         success: callback;
         error: callback;
     */
@@ -207,9 +212,9 @@ class ncentSDK {
             console.log(error.response.data);
         });
     }
-
+    
     /*
-        getTokenTransactionHistory provides a history of outgoing and incoming
+        getTokenTransactionHistory provides a history of outgoing and incoming 
         transactions from a user's wallet for one token.
         string walletAddress: user's wallet address.
         int tokenType: UUID for token.
@@ -231,11 +236,11 @@ class ncentSDK {
     //         console.log(error);
     //     })
     //     .then(function(){
-    //     });
+    //     });       
     // }
-
+    
     // /*
-    //     getTransactionHistory provides a history of outgoing and incoming
+    //     getTransactionHistory provides a history of outgoing and incoming 
     //     transactions from a user's wallet for one token.
     //     string walletAddress: user's wallet address.
     //     params: any additional parameters.
@@ -256,12 +261,12 @@ class ncentSDK {
     //     })
     //     .then(function(){
 
-    //     });
+    //     });   
     // }
-
+    
     /*
-        redeemToken redeems the token based off the specified redeem action
-        written in the original contract.
+        redeemToken redeems the token based off the specified redeem action 
+        written in the original contract. 
         string walletAddress: creator's wallet address.
         string redeemerSignature: redeemer's signature.
         string sponsorSignature: sponsor signature.
@@ -280,14 +285,14 @@ class ncentSDK {
     //        })
     //        .catch(function(error){
     //            console.log(error);
-    //        });
+    //        });   
     // }
-
+    
     // /*
-    //     signMessage adds signature to a message object.
+    //     signMessage adds signature to a message object. 
     //     success: callback.
     //     error: callback.
-    // */
+    // */   
     // signMessage (params, success, error) {
     //     axios.post(this._net + '/transaction')
     //         .then(function(response){
@@ -296,23 +301,18 @@ class ncentSDK {
     //         .catch(function(error){
     //             console.log(error);
     //         });
-    // }
+    // }        
 }
 var that = new ncentSDK();
-//that.createWallet("kd@gmail.com");
 //that.inIt("kd@gmail.com", 090, 'success', 'error');
 //that.destroyTokens('hi@hi.com', 'password', 001, success, error);
-//that.stampToken('an@ncnt.io', "jobCent", 100, '2018-09-09');
+//var tokentype_id = that.stampToken('an@ncnt.io', "jobCent", 100, '2018-09-09');
 //that.createWallet("kyle@ncnt.io");
-//that.createWallet("jd@ncnt.io");
-//that.destroyTokens('7b139b34-aea8-446a-b1eb-182ab169f664');
+//that.createWallet("jd@ncnt.io");   
+//that.destroyTokens(tokentype_id);
 
-
-//that.checkIfBalanceExists('jd@ncnt.io', '59d6533f-259e-4864-bf31-9538151b84b1');
-//that.createBalance('jd@ncnt.io', '59d6533f-259e-4864-bf31-9538151b84b1');
-//that.checkIfBalanceExists('kyle@ncnt.io', '59d6533f-259e-4864-bf31-9538151b84b1');
-//that.getTokenBalance('jd@ncnt.io', '59d6533f-259e-4864-bf31-9538151b84b1');
-//that.getAllBalances('59d6533f-259e-4864-bf31-9538151b84b1');
-that.transferTokens("jd@ncnt.io", "kyle@ncnt.io", '59d6533f-259e-4864-bf31-9538151b84b1', 100);
-
-module.exports = ncentSDK;
+//var balance_id = that.createBalance('jd@ncnt.io', '587e0c4b-97b8-4202-9a8f-9ad30e63fdb5');
+//that.getTokenBalance('jd@ncnt.io', 'dcd96451-575f-48c4-a114-dba00e7350a9');
+//that.getAllBalances('jd@ncnt.io');
+that.transferTokens('dcd96451-575f-48c4-a114-dba00e7350a9', null, "jd@ncnt.io", "kyle@ncnt.io", '587e0c4b-97b8-4202-9a8f-9ad30e63fdb5', 100);
+    
