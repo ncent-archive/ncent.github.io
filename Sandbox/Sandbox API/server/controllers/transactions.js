2fef8c4bf6ffd6ea1cf70cf5249742c966d41324
admin <admin@admins-MacBook-Pro.local>
2018-07-23 13:15:15 -0700

    api changes

--- Sandbox/Sandbox API/server/controllers/transactions.js
+++ Sandbox/Sandbox API/server/controllers/transactions.js
@@ -1,17 +1,63 @@
 const Transaction = require('../models').Transaction;
+const Wallet = require('../models').Wallet;
 
 module.exports = {
   create(req, res) {
-    return Transaction
+    let data;
+    Transaction
       .create({
         amount: req.body.amount,
         fromAddress: req.body.fromAddress,
         toAddress: req.body.toAddress,
         tokentype_uuid: req.params.tokentype_uuid
       })
-      .then(transaction => res.status(201).send(transaction))
+      .then(transaction => {data = {txn: transaction}})
       .catch(error => res.status(400).send(error));
-  },
+    Wallet
+      .findAll({
+        where: {
+          wallet_uuid: req.body.fromAddress,
+          tokentype_uuid: req.params.tokentype_uuid,
+        }
+      })
+      .then(wallets => {
+        if (!wallets || wallets.length < 1 ) {
+          return res.status(404).send({
+            message: 'Balance for Wallet Not Found',
+          });
+        }
+        wallets[0].update({
+            balance: parseInt(wallets[0].balance, 10) - parseInt(req.body.amount, 10),
+          })
+          .then((wallets) => {data["sender"] = wallets})  // Send back the updated wallet.
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error))
+    Wallet
+      .findAll({
+        where: {
+          wallet_uuid: req.body.toAddress,
+          tokentype_uuid: req.params.tokentype_uuid,
+        }
+      })
+      .then(wallets => {
+        if (!wallets || wallets.length < 1 ) {
+          return res.status(404).send({
+            message: 'Balance for Wallet Not Found',
+          });
+        }
+        wallets[0].update({
+            balance: parseInt(wallets[0].balance, 10) + parseInt(req.body.amount, 10),
+          })
+          .then((wallets) => {
+            console.log(data)
+            data["receiver"] = wallets
+            return res.status(200).send(data);
+          })  // Send back the updated wallet.
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error))
+    },
   list(req, res) {
     return Transaction
       .findAll({
