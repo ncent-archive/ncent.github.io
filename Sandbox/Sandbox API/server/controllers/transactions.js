58a6c6c9ceec4bd97a820aab4632fde1f729b203
admin <admin@admins-MacBook-Pro.local>
2018-07-23 17:25:14 -0700

    messages

--- Sandbox/Sandbox API/server/controllers/transactions.js
+++ Sandbox/Sandbox API/server/controllers/transactions.js
@@ -11,51 +11,55 @@ module.exports = {
         toAddress: req.body.toAddress,
         tokentype_uuid: req.params.tokentype_uuid
       })
-      .then(transaction => {data = {txn: transaction}})
+      .then(transaction => {
+        data = {txn: transaction}
+        Wallet
+          .findAll({
+            where: {
+              wallet_uuid: req.body.fromAddress,
+              tokentype_uuid: req.params.tokentype_uuid,
+            }
+          })
+          .then(wallets => {
+            if (!wallets || wallets.length < 1 ) {
+              return res.status(404).send({
+                message: 'Balance for Wallet Not Found',
+              });
+            }
+            wallets[0].update({
+                balance: parseInt(wallets[0].balance, 10) + parseInt(req.body.amount, 10),
+              })
+              .then((wallets) => {
+                data["sender"] = wallets[0]
+                Wallet
+                  .findAll({
+                    where: {
+                      wallet_uuid: req.body.toAddress,
+                      tokentype_uuid: req.params.tokentype_uuid,
+                    }
+                  })
+                  .then(wallets => {
+                    if (!wallets || wallets.length < 1 ) {
+                      return res.status(404).send({
+                        message: 'Balance for Wallet Not Found',
+                      });
+                    }
+                    wallets[0].update({
+                        balance: parseInt(wallets[0].balance, 10) + parseInt(req.body.amount, 10),
+                      })
+                      .then((wallets) => {
+                        data["receiver"] = wallets[0]
+                        return res.status(200).send(data);
+                      })  // Send back the updated wallet.
+                      .catch((error) => res.status(400).send(error));
+                  })
+                  .catch(error => res.status(400).send(error))
+              })  // Send back the updated wallet.
+              .catch((error) => res.status(400).send(error));
+          })
+          .catch(error => res.status(400).send(error))
+      })
       .catch(error => res.status(400).send(error));
-    Wallet
-      .findAll({
-        where: {
-          wallet_uuid: req.body.fromAddress,
-          tokentype_uuid: req.params.tokentype_uuid,
-        }
-      })
-      .then(wallets => {
-        if (!wallets || wallets.length < 1 ) {
-          return res.status(404).send({
-            message: 'Balance for Wallet Not Found',
-          });
-        }
-        wallets[0].update({
-            balance: parseInt(wallets[0].balance, 10) + parseInt(req.body.amount, 10),
-          })
-          .then((wallets) => {data["sender"] = wallets[0]})  // Send back the updated wallet.
-          .catch((error) => res.status(400).send(error));
-      })
-      .catch(error => res.status(400).send(error))
-    Wallet
-      .findAll({
-        where: {
-          wallet_uuid: req.body.toAddress,
-          tokentype_uuid: req.params.tokentype_uuid,
-        }
-      })
-      .then(wallets => {
-        if (!wallets || wallets.length < 1 ) {
-          return res.status(404).send({
-            message: 'Balance for Wallet Not Found',
-          });
-        }
-        wallets[0].update({
-            balance: parseInt(wallets[0].balance, 10) + parseInt(req.body.amount, 10),
-          })
-          .then((wallets) => {
-            data["receiver"] = wallets[0]
-            return res.status(200).send(data);
-          })  // Send back the updated wallet.
-          .catch((error) => res.status(400).send(error));
-      })
-      .catch(error => res.status(400).send(error))
     },
   list(req, res) {
     return Transaction
