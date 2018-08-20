79648742be0ddb21af501fa6c3b89705fce19477
Adam Foosaner <nucat176@gmail.com>
2018-08-20 15:49:20 -0700

    removes jobCent database for testing

--- Applications/jobCentDatabase/server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const usersController = require('../controllers').users;
-
-module.exports = (app) => {
-    app.post('/jobCent/users', usersController.create);
-    app.get('/jobCent/users', usersController.list);
-    app.get('/jobCent/users/:id', usersController.retrieve);
-};
